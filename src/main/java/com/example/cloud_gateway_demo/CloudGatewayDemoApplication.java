47dc212ebe82a1da4ec6e76a6b8211636cc23f56
Adix411 <adix411@wp.pl>
2024-10-05 12:17:38 +0200

    commit

---
+++ src/main/java/com/example/cloud_gateway_demo/CloudGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud_gateway_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloud_gateway_demo/CloudGatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud_gateway_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
